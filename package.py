8c0289272d5d96f9cee32d13550a67e0cfcf0720
Antoine Labonte <alabont@microsoft.com>
2025-01-15 15:17:42 -0500

    Adding automated release on tags

--- package.py
+++ package.py
@@ -17,7 +17,7 @@ CYCLECLOUD_API_VERSION = "8.1.0"
 def build_sdist() -> str:
     cmd = [sys.executable, "setup.py", "sdist", "--formats=zip"]
     check_call(cmd, cwd=os.path.abspath("hpcpack-autoscaler"))
-    sdists = glob.glob("hpcpack-autoscaler/dist/cyclecloud-hpcpack-*.zip")
+    sdists = glob.glob("hpcpack-autoscaler/dist/cyclecloud_hpcpack-*.zip")
     assert len(sdists) == 1, "Found %d sdist packages, expected 1" % len(sdists)
     path = sdists[0]
     fname = os.path.basename(path)
